98208a4118bded14d23cf740098e65c5552fc8b0
meep_solp <meep_solp@10.1.4.248>
2017-01-04 10:03:30 +0100

    Initial version

---
+++ Pointers/src/Pointers.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : Pointers.cpp
+// Author      : Pawel
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <memory>
+using namespace std;
+
+int main()
+{
+
+	string* pWord = 0;
+	unique_ptr<string> pWord2 = 0;
+
+	pWord = new string("Helo");
+
+	cout << *pWord << endl;
+
+	delete pWord;
+
+	return 0;
+}
