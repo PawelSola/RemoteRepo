9f694013597865804775d1df9c0540ba0190dc31
meep_solp <meep_solp@10.1.4.248>
2017-01-10 11:16:39 +0100

    First commit

--- Pointers/src/Pointers.cpp
+++ Pointers/src/Pointers.cpp
@@ -8,7 +8,28 @@
 
 #include <iostream>
 #include <memory>
+#include <map>
 using namespace std;
+
+
+template <class T>
+void f(T)
+{
+
+}
+
+template <class T>
+void g(const T&)
+{
+
+}
+
+template <class T>
+void h(T&)
+{
+
+}
+
 
 int main()
 {
@@ -19,6 +40,59 @@ int main()
 	pWord = new string("Helo");
 
 	cout << *pWord << endl;
+
+
+	auto* x = new int;
+
+	cout << std::is_same<int*, decltype(x)>::value << endl;
+//	cout << "Type: " <<  << endl;
+
+	std::map<int,int> Mymap;
+	Mymap[0] = 0;
+
+	for ( const std::pair<const int, int>& entry : Mymap)
+	{
+
+	}
+
+	//C++ 11
+	for ( const auto& entry : Mymap)
+	{
+
+	}
+
+	//C++ 14
+//	for ( decltype(auto) entry : Mymap)
+//	{
+//
+//	}
+
+	int i = 0;
+	const int ci = 2;
+	int& i_ref = i;
+	const int& ci_ref = ci;
+
+	int a = 0;
+	int& b = a;
+	int&& c = 5;
+
+	int&& d = std::move(c);
+	int&& e = static_cast<int&&>(d);
+
+	auto x0 = i;
+	auto x1 = ci;
+
+	cout << std::is_const<decltype(ci)>::value << endl;
+
+	f(i);
+	f(ci);
+
+	g(i);
+	g(ci);
+
+	h(i);
+	h(ci);
+
 
 	delete pWord;
 
